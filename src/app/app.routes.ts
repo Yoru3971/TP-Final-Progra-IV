21dc1b9451137a364d313ff3a186896880d1adf4
Emiliano Daverio <emilianodaverio@gmail.com>
2025-11-05 17:05:57 -0300

    Preparacion pages
    - Se borra todo el placeholder de la pagina principal y se conecta la ruta de la pagina del registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { RegisterPageDueno } from './pages/register-page-dueno/register-page-dueno';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'register/dueno', component: RegisterPageDueno},
+];
