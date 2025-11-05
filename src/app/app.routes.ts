7b946f74492f864c869bce3362a968bb2cc17982
Emiliano Daverio <emilianodaverio@gmail.com>
2025-11-05 17:44:47 -0300

    Lectura de ruta desde URL
    - El rol se levanta directamente desde la url y se pasa automaticamente al form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { RegisterPageDueno } from './pages/register-page-dueno/register-page-dueno';
 
 export const routes: Routes = [
-    {path: 'register/dueno', component: RegisterPageDueno},
+    {path: 'registro/:rol', component: RegisterPageDueno},
 ];
