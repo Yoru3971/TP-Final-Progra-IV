cf00136ec53bea875cbd23cc49d911d9135a6dc1
kevinraipane <162730749+kevinraipane@users.noreply.github.com>
2025-11-16 02:54:06 -0300

    test - pagina para probar el form
    Solo se creo con el fin de testear el form

---
+++ src/app/pages/crear-emprendimiento-page-dueno/crear-emprendimiento-page-dueno.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FormEmprendimiento } from "../../components/form-emprendimiento/form-emprendimiento";
+
+@Component({
+  selector: 'app-crear-emprendimiento-page-dueno',
+  imports: [FormEmprendimiento],
+  templateUrl: './crear-emprendimiento-page-dueno.html',
+  styleUrl: './crear-emprendimiento-page-dueno.css',
+})
+export class CrearEmprendimientoPageDueno {
+
+}
