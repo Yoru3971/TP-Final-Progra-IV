a52114f4a82e135612b892a0f9f6b9a91f56a00c
Emiliano Daverio <emilianodaverio@gmail.com>
2025-11-17 01:21:37 -0300

    feat- base pedidos card

---
+++ src/app/components/notificaciones-card/notificaciones-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notificaciones-card',
+  imports: [],
+  templateUrl: './notificaciones-card.html',
+  styleUrl: './notificaciones-card.css',
+})
+export class NotificacionesCard {
+
+}
