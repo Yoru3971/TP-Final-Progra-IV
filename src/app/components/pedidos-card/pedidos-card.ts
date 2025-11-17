6f86cb72c83e581e010a9c9cff574b46d5979bb3
Emiliano Daverio <emilianodaverio@gmail.com>
2025-11-17 01:21:47 -0300

    feat- base pedidos card

---
+++ src/app/components/pedidos-card/pedidos-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pedidos-card',
+  imports: [],
+  templateUrl: './pedidos-card.html',
+  styleUrl: './pedidos-card.css',
+})
+export class PedidosCard {
+
+}
