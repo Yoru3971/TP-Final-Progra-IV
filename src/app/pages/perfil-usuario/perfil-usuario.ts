935ecfad84be3e67d273ffc0949583113f86f912
Emiliano Daverio <emilianodaverio@gmail.com>
2025-11-17 01:21:56 -0300

    feat - perfil usuario page

--- src/app/pages/perfil-usuario/perfil-usuario.ts
+++ src/app/pages/perfil-usuario/perfil-usuario.ts
@@ -1,8 +1,11 @@
-import { Component, Input} from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { DatosUsuarioCard } from '../../components/datos-usuario-card/datos-usuario-card';
 import { NotificacionesCard } from '../../components/notificaciones-card/notificaciones-card';
 import { PedidosCard } from '../../components/pedidos-card/pedidos-card';
 import { PanelAccionesCuenta } from '../../components/panel-acciones-cuenta/panel-acciones-cuenta';
+import { UsuarioResponse } from '../../model/usuario-response.model';
+import { UsuarioService } from '../../services/usuario-service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-perfil-usuario',
@@ -10,6 +13,18 @@ import { PanelAccionesCuenta } from '../../components/panel-acciones-cuenta/pane
   templateUrl: './perfil-usuario.html',
   styleUrl: './perfil-usuario.css',
 })
-export class PerfilUsuario {
+export class PerfilUsuario implements OnInit {
+  private usuarioService = inject(UsuarioService);
 
+  usuario = signal<UsuarioResponse | null>(null);
+
+  //REVISAR manejo de errores con modal
+  ngOnInit(): void {
+    this.usuarioService.getPerfilUsuario().subscribe({
+      next: (data) => {
+        this.usuario.set(data);
+      },
+      error: (err) => console.error(err),
+    });
+  }
 }
