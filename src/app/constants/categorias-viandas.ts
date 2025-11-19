d145ecc0e28f692e6609d97a8666e90df820bbee
kevinraipane <162730749+kevinraipane@users.noreply.github.com>
2025-11-19 00:43:48 -0300

    feat - model vianda create y enum de categorias

---
+++ src/app/constants/categorias-viandas.ts
@@ -0,0 +1,23 @@
+export const CATEGORIAS_VIANDA = [
+  { key: "MENU_DEL_DIA", label: "Menú del día" },
+  { key: "MILANESA", label: "Milanesas" },
+  { key: "PASTA", label: "Pastas" },
+  { key: "PARRILLA", label: "Parrilla" },
+  { key: "WOK", label: "Woks y salteados" },
+  { key: "TARTA", label: "Tartas" },
+  { key: "EMPANADA", label: "Empanadas" },
+  { key: "ENSALADA", label: "Ensaladas" },
+  { key: "SOPA", label: "Sopas" },
+  { key: "PARA_COMPARTIR", label: "Para compartir" },
+  { key: "SUSHI", label: "Sushi" },
+  { key: "ARABE", label: "Comida árabe" },
+  { key: "JAPONESA", label: "Comida japonesa" },
+  { key: "CHINA", label: "Comida china" },
+  { key: "PESCADO", label: "Pescados y mariscos" },
+  { key: "HAMBURGUESA", label: "Hamburguesas" },
+  { key: "POSTRE", label: "Postres" },
+  { key: "PIZZA", label: "Pizzas" },
+  { key: "SANDWICH", label: "Sandwiches" }
+];
+
+
---
+++ src/app/model/vianda-create.model.ts
@@ -0,0 +1,11 @@
+export interface ViandaCreate {
+  nombreVianda: string;
+  categoria: string;        
+  descripcion: string;
+  image: File | null;             
+  precio: number;
+  esVegano: boolean;
+  esVegetariano: boolean;
+  esSinTacc: boolean;
+  emprendimientoId: number;
+}
