e5a227ae4627fde7ebfc75b4be3471448dbf2341
Tomas-Kauand <kauandtomas@gmail.com>
2025-11-21 00:52:04 -0300

    Creación Card de vianda detallada
    - Se creó la vianda-card-detallada usada en la página de un emprendimiento
    - más adelante se mejoran los estilos y los iconos

--- src/app/components/vianda-card-detallada/vianda-card-detallada.ts
+++ src/app/components/vianda-card-detallada/vianda-card-detallada.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { ViandaResponse } from '../../model/vianda-response.model';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-vianda-card-detallada',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './vianda-card-detallada.html',
   styleUrl: './vianda-card-detallada.css',
 })
 export class ViandaCardDetallada {
+
+  // REVISAR si esto queda así o va en otro lado (carrito)
+  vianda = input.required<ViandaResponse>();
+  modo = input.required<'dueno' | 'cliente'>();
+  
+  cantidad = input<number>(0); 
+
+  editar = output<ViandaResponse>(); 
+  cantidadChange = output<number>();
+
+  esDueno() {
+    return this.modo() === 'dueno';
+  }
+
+  incrementar() {
+    const nuevaCantidad = this.cantidad() + 1;
+    this.cantidadChange.emit(nuevaCantidad);
+  }
+
+  decrementar() {
+    if (this.cantidad() > 0) {
+      const nuevaCantidad = this.cantidad() - 1;
+      this.cantidadChange.emit(nuevaCantidad);
+    }
+  }
 
 }
