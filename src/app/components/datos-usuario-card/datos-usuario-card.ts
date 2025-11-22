49139aefa63db2b7c366cf550e98cb6c4f6d902a
kevinraipane <162730749+kevinraipane@users.noreply.github.com>
2025-11-22 16:07:09 -0300

    fix - newSignal
    Creo un signal con los datos del @Input

--- src/app/components/datos-usuario-card/datos-usuario-card.ts
+++ src/app/components/datos-usuario-card/datos-usuario-card.ts
@@ -1,5 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input, OnInit, signal } from '@angular/core';
 import { UsuarioResponse } from '../../model/usuario-response.model';
+import { MatDialog } from '@angular/material/dialog';
+import { FormUserUpdate } from '../form-user-update/form-user-update';
+import { UsuarioService } from '../../services/usuario-service';
 
 @Component({
   selector: 'app-datos-usuario-card',
@@ -7,9 +10,37 @@ import { UsuarioResponse } from '../../model/usuario-response.model';
   templateUrl: './datos-usuario-card.html',
   styleUrl: './datos-usuario-card.css',
 })
-export class DatosUsuarioCard {
+export class DatosUsuarioCard implements OnInit{
+  private usuarioService = inject(UsuarioService);
+
   @Input() usuario!: UsuarioResponse;
 
-  //REVISAR aca deberia ir el metodo de editar datos usuario
+  public usuarioSignal = signal<UsuarioResponse>(this.usuario);
 
+  ngOnInit(): void {
+    if (this.usuario) {
+      this.usuarioSignal.set(this.usuario);
+      console.log(this.usuarioSignal()); //REVISAR quitar este log, solo de test
+    }
+  }
+
+  private dialog = inject(MatDialog);
+
+  openUpdateModal() {
+    const dialogRef = this.dialog.open(FormUserUpdate, {
+          data: this.usuario,
+          panelClass: 'modal-vianda',
+          autoFocus: false,
+          restoreFocus: false,
+        });
+    dialogRef.afterClosed().subscribe(result => {
+      this.usuarioService.getPerfilUsuario().subscribe({
+      next: (data) => {
+        this.usuarioSignal.set(data);
+      },
+      error: (err) => console.error(err),
+    });
+    })
+  }
+  
 }
