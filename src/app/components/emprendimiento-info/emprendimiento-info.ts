00a6643cf92a31aee6ee0e7c4b6138c6bfde9e67
kevinraipane <162730749+kevinraipane@users.noreply.github.com>
2025-11-22 19:52:57 -0300

    Correcion de campos

--- src/app/components/emprendimiento-info/emprendimiento-info.ts
+++ src/app/components/emprendimiento-info/emprendimiento-info.ts
@@ -1,5 +1,17 @@
-import { Component, EventEmitter, inject, input, Input, output, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  inject,
+  input,
+  Input,
+  output,
+  Output,
+  signal,
+} from '@angular/core';
 import { EmprendimientoResponse } from '../../model/emprendimiento-response.model';
+import { MatDialog } from '@angular/material/dialog';
+import { FormUpdateEmprendimiento } from '../form-emprendimiento-update/form-emprendimiento-update';
+import { EmprendimientoService } from '../../services/emprendimiento-service';
 
 @Component({
   selector: 'app-emprendimiento-info',
@@ -8,13 +20,41 @@ import { EmprendimientoResponse } from '../../model/emprendimiento-response.mode
   styleUrl: './emprendimiento-info.css',
 })
 export class EmprendimientoInfo {
+  private emprendimientoService = inject(EmprendimientoService);
 
-  datos = input.required<EmprendimientoResponse>();
-  modo = input.required<'CLIENTE' | 'DUENO'| 'INVITADO' | 'PROHIBIDO' | 'CARGANDO'>();
+  @Input() datos!: EmprendimientoResponse;
+  public emprendimientoSignal = signal<EmprendimientoResponse | null>(null);
+
+  ngOnInit(): void {
+    if (this.datos) {
+      this.emprendimientoSignal.set(this.datos);
+    }
+  }
+
+  modo = input.required<'CLIENTE' | 'DUENO' | 'INVITADO' | 'PROHIBIDO' | 'CARGANDO'>();
+
+  private dialog = inject(MatDialog);
 
   accionPrincipal = output<void>();
 
   onButtonClick() {
     this.accionPrincipal.emit();
+  }
+
+  openEmprendimientoForm() {
+    this.dialog
+      .open(FormUpdateEmprendimiento, {
+        width: '100rem',
+        panelClass: 'form-modal',
+        autoFocus: false,
+        restoreFocus: false,
+        data: this.datos
+      })
+      .afterClosed()
+      .subscribe((exito) => {
+        if (exito) {
+          this.emprendimientoService.fetchEmprendimientos();
+        }
+      });
   }
 }
