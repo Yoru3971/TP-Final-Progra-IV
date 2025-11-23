0b8b4e12cfb75e9d043c3bb9bd3155f97c7244e7
kevinraipane <162730749+kevinraipane@users.noreply.github.com>
2025-11-23 03:10:01 -0300

    update - pedidoExtendModal

--- src/app/components/pedidos-card/pedidos-card.ts
+++ src/app/components/pedidos-card/pedidos-card.ts
@@ -2,7 +2,6 @@ import { Component, inject, OnInit } from '@angular/core';
 import { PedidosService } from '../../services/pedido-service';
 import { PedidoSingleCard } from '../pedido-single-card/pedido-single-card';
 import { DateRangePickerComponent } from '../../shared/components/date-range-picker/date-range-picker';
-import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { EstadoPedido } from '../../shared/enums/estadoPedido.enum';
 
 @Component({
--- src/app/model/pedido-update-request.model.ts
+++ src/app/model/pedido-update-request.model.ts
@@ -1,6 +1,7 @@
 import { EstadoPedido } from "../shared/enums/estadoPedido.enum";
 
 export interface PedidoUpdateRequest {
-  estado: EstadoPedido
-  fechaEntrega: string; // yyyy-MM-dd
+  estado?: EstadoPedido | null;
+  fechaEntrega?: string | null;
 }
+
