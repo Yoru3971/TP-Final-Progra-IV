7abc8d87557c58d5554b1e5daef6c240cb6bce78
Emiliano Daverio <emilianodaverio@gmail.com>
2025-11-22 21:50:44 -0300

    feat-snack exito modal error

--- src/app/components/pedido-extended-modal/pedido-extended-modal.ts
+++ src/app/components/pedido-extended-modal/pedido-extended-modal.ts
@@ -5,6 +5,7 @@ import {
   MatDialogContent,
   MatDialogClose,
   MatDialogRef,
+  MatDialog,
 } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -15,6 +16,9 @@ import { PedidosService } from '../../services/pedido-service';
 import { PedidoUpdateRequest } from '../../model/pedido-update-request.model';
 import { EstadoPedido } from '../../shared/enums/estadoPedido.enum';
 import { UsuarioResponse } from '../../model/usuario-response.model';
+import { Snackbar } from '../../shared/components/snackbar/snackbar';
+import { ErrorDialogModal } from '../../shared/components/error-dialog-modal/error-dialog-modal';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-pedido-extended-modal',
@@ -35,23 +39,44 @@ export class PedidoExtendedModal {
   private pedidosService = inject(PedidosService);
   private dialogRef = inject(MatDialogRef);
   public role = this.authService.currentUserRole;
+  private dialog = inject(MatDialog);
+  private snackBar = inject(MatSnackBar);
   constructor(@Inject(MAT_DIALOG_DATA) public pedido: PedidoResponse) {}
-
+  
   cambiarEstadoPedido(estadoUpd: EstadoPedido) {
     const body: PedidoUpdateRequest = {
       estado: estadoUpd,
-      fechaEntrega: this.pedido.fechaEntrega, // debe ser yyyy-MM-dd
+      fechaEntrega: this.pedido.fechaEntrega,
     };
+    console.log("ID:", this.pedido.id, "Estado:", estadoUpd);
 
     this.pedidosService.updatePedido(this.pedido.id, body).subscribe({
       next: () => {
+        const data = {
+          message: 'Pedido actualizado correctamente',
+          iconName: 'check_circle',
+        };
+
+        this.snackBar.openFromComponent(Snackbar, {
+          duration: 3000,
+          verticalPosition: 'bottom',
+          panelClass: 'snackbar-panel',
+          data,
+        });
+
         setTimeout(() => {
           this.pedidosService.fetchPedidos();
         }, 500);
-        this.dialogRef.close();
+
+        this.dialogRef.close({ updated: true });
       },
       error: (err) => {
-        console.error('Error al actualizar pedido', err);
+        this.dialog.open(ErrorDialogModal, {
+          data: {
+            message: err.error?.message ?? 'Error desconocido al actualizar el pedido',
+          },
+          panelClass: 'modal-error',
+        });
       },
     });
   }
