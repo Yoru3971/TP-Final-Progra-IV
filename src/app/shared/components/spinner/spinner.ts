9a2f1f2be7a3a8b7c6978fd66e118ce9f0ff3d14
Emiliano Daverio <emilianodaverio@gmail.com>
2025-11-24 01:10:24 -0300

    feat- loading spinner

---
+++ src/app/shared/components/spinner/spinner.ts
@@ -0,0 +1,13 @@
+import { Component, inject } from '@angular/core';
+import { LoadingService } from '../../../services/loading-service';
+
+@Component({
+  selector: 'app-spinner',
+  imports: [],
+  templateUrl: './spinner.html',
+  styleUrl: './spinner.css',
+})
+export class Spinner {
+  private loadingService = inject(LoadingService);
+  loading = this.loadingService.loading;
+}
