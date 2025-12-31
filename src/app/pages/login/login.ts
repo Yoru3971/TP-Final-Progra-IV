0ef3d93969c3208c918a2e24e696ad52bb901184
kevinraipane <162730749+kevinraipane@users.noreply.github.com>
2025-12-31 11:55:51 -0300

    update - loginPage
    Agregamos el boton de login con Gmail a la pagina Login

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FormLogin } from '../../components/forms/form-login/form-login';
 import { RouterLink } from '@angular/router';
+import { GoogleLoginButton } from '../../components/utils/google-login-button/google-login-button';
 
 @Component({
   selector: 'app-login',
-  imports: [FormLogin, RouterLink],
+  imports: [FormLogin, GoogleLoginButton, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
