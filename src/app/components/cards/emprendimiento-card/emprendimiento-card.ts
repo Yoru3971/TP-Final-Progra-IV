87e0d748c2da8ef270cc050706cf79826ef159af
Tomas-Kauand <kauandtomas@gmail.com>
2026-01-08 20:30:40 -0300

    Fix Emprendimiento Card
    - ahora se muestra correctamente el icono para emprendimientos cuando falla el logo

--- src/app/components/cards/emprendimiento-card/emprendimiento-card.ts
+++ src/app/components/cards/emprendimiento-card/emprendimiento-card.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
 import { EmprendimientoConViandas } from '../../../model/emprendimiento-con-viandas.model';
 import { ViandaCard } from '../vianda-card/vianda-card';
 import { RouterLink } from '@angular/router';
@@ -11,6 +11,12 @@ import { RouterLink } from '@angular/router';
 })
 export class EmprendimientoCard {
   @Input() emprendimiento!: EmprendimientoConViandas;
+
+  imageLoadError = signal(false);
+
+  handleImageError() {
+    this.imageLoadError.set(true);
+  }
 
   getallViandas() {
     return this.emprendimiento.viandas;
